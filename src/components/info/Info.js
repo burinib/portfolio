6490937615dc3da7e6a308a00d209c870f50a07b
Braian Burini <braianb55@hotmail.com>
2022-10-28 19:27:21 -0300

    sumando certificados

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import "./Info.css";
+import certificadoDW from "../../media/certificadoDW.png";
+import certificadoJavaScript from "../../media/certificadoJavaScript.png";
+import certificadoReact from "../../media/certificadoReact.png";
 
 const Info = () => {
   return (
+    
     <div className='info-container'>
+      <h1 className='titleCertificado'>Mis certificados</h1>
     <div className='info'>
-      <div>
-        <h1 className='infoTrabajar'>Trabajemos juntos</h1>
+        <div className='certificado'>
+        <img className="desarrolloWeb" src={certificadoDW} alt="" />
         </div>
-        <div>
-          <h1 className='infoCertificado'>Mis certificados</h1>
+        <div className='certificado'>
+        <img className="desarrolloWeb" src={certificadoJavaScript} alt="" />
+        </div>
+        <div className='certificado'>
+        <img className="desarrolloWeb" src={certificadoReact} alt="" />
         </div>
     </div>
 
