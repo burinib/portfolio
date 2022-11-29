64aada4ea34ed53246c8790e333db169f5482227
Braian Burini <braianb55@hotmail.com>
2022-11-29 16:17:08 -0300

    sumando pdf

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import imgPersonal from "../../media/imgPersonal.jpg";
+import descarga from "../../media/CvBuriniBraian.pdf";
 
 import "./About.css";
 
@@ -8,7 +9,7 @@ const About = () => {
     <div className="about-container">
       <div className="about-desc">
         <h3>Sobre mi</h3>
-        <p>
+      {/*   <p>
           <h2>Informacion personal:</h2>
           <p>Nac: 20-09-1991 </p>
           <p>D.N.I: 36.262.117 </p>
@@ -32,7 +33,13 @@ const About = () => {
       <h4>13-06-2020 - Actual T.A.S.A</h4>
       <p>Operario polivalente de la empresa Toyota ubicada en Zarate.</p>
       <p>Me ecuentro desarrollando tareas en el area de Motores, cumpliendo con los objetivos que dicha empresa necesita, trabajando en equipo y con buena predisposicion para seguir aprendiendo dia a dia</p>
-
+ */}
+        <p>¡Hola! Mi nombre es Braian, Soy desarrollador de aplicaciones desktop, web y móvil, abocado especificamente en el area de Front-End</p>
+        <p>Apasionado por las tecnologías web, día a día me esfuerzo para mejorar mis capacidades y obtener nuevos conocimientos.</p>
+        <div className="descarga">
+          <a href={descarga}
+          download>Descargar mi CV</a>
+        </div>
       </div>
       <div className="about-img">
         <img className="img-personal" src={imgPersonal} alt="about" />
